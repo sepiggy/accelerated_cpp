5ab2c78569546a1e5a99bff20041edf60b394680
sepiggy <jms1209@qq.com>
2018-04-12 10:58:10 +0800

    b4 ch0.6 ok

--- chapter00/hello.cc
+++ chapter00/hello.cc
@@ -1,9 +1,9 @@
 // a small C++ program
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, world!" << std::endl;
-	return 0;
+int main() {
+  std::cout << "Hello, world!" << std::endl;
+  std::cout << "Hello, world again";
+  std::cout << " I am here in the same line" << std::endl;
+  return 0;
 }
-
